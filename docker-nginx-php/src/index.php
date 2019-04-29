593e09d0d9c53c497a84dee9a731d08c1bd98d67
Nikita Nyatin <yesnik@yandex.ru>
2019-04-29 17:58:09 +0500

    Add docker-nginx-php

---
+++ docker-nginx-php/src/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Nginx + PHP-FPM</title>
+    <script src="/script.js"></script>
+  </head>
+  <body>
+<?php
+
+echo "Hello, current time is: " . date('Y-m-d H:i:s');
+
+?>
+  </body>
+</html>
